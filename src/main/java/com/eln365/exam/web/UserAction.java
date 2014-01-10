b9cc64ae6b859413963095dd92943df0df828f8b
steven yang <ya888g@163.com>
2014-01-10 17:29:18 +0800

    初始代码

---
+++ src/main/java/com/eln365/exam/web/UserAction.java
@@ -0,0 +1,20 @@
+package com.eln365.exam.web;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author yangqinglei E-mail: yangqinglei@gw.com.cn
+ * @version 创建时间：2014-1-10 下午3:28:39 类说明
+ */
+@SuppressWarnings("serial")
+public class UserAction extends ActionSupport {
+	public String login() {
+		 double CID = 0x00d8;
+		return "login";
+	}
+	
+	public String index() {
+
+		return "index";
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2> welcome to index.jsp!</h2>
+</body>
+</html>
